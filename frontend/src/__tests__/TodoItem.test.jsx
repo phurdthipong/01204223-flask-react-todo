48852a87ac49e9999e09a395af42e93774899847
phurdthipong choktana a nun <phurdthipong.c@ku.th>
2026-02-13 15:05:08 +0700

    testcase 0

---
+++ frontend/src/__tests__/TodoItem.test.jsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react'
+import { expect } from 'vitest'
+import TodoItem from '../TodoItem.jsx'
+
+describe('TodoItem', () => {
+  it('renders with no comments correctly', () => {
+    // เดี๋ยวจะเพิ่มโค้ดตรงนี้
+  });
+});
